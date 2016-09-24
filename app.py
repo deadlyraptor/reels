b698e851bbea27a12489fc951c2a0fd352835db4
Javier Chavez <jachavez6@gmail.com>
2016-09-24 18:35:48 -0400

    Basic reading and writing is working.
    As is, the script will copy all of the contents of the rows containing
    the first name, last name, and email address of the patron from the
    Agile .xls file and to a new file called 'Email Adds.xls'.
    
    There are various bugs in this initial commit and missing functionality.

---
+++ app.py
@@ -0,0 +1,66 @@
+import xlrd
+import xlwt
+import settings
+
+wb = xlrd.open_workbook(settings.location)
+sh = wb.sheet_by_index(0)
+total_rows = sh.nrows
+first_row = 6
+
+fname = []
+def firstName():
+    rowx = 6
+    colx = 2
+    for row in range(first_row, total_rows):
+        fname.append(sh.cell_value(rowx, colx))
+        rowx += 1
+    print(fname)
+
+lname = []
+def lastName():
+    rowx = 6
+    colx = 1
+    for row in range(first_row, total_rows):
+        lname.append(sh.cell_value(rowx, colx))
+        rowx += 1
+    print(lname)
+
+elist = []
+def email():
+    rowx = 6
+    colx = 3
+    for row in range(first_row, total_rows):
+        elist.append(sh.cell_value(rowx, colx))
+        rowx += 1
+    print(elist)
+
+firstName()
+lastName()
+email()
+
+book = xlwt.Workbook()
+sheet = book.add_sheet('Email Adds')
+
+
+def wfname():
+    column_number = 0
+    for row_number, item in enumerate(fname):
+        sheet.write(row_number, column_number, item)
+
+
+def wlname():
+    column_number = 1
+    for row_number, item in enumerate(lname):
+        sheet.write(row_number, column_number, item)
+
+
+def wemail():
+    column_number = 2
+    for row_number, item in enumerate(elist):
+        sheet.write(row_number, column_number, item)
+
+wfname()
+wlname()
+wemail()
+
+book.save('Email Adds.xls')
