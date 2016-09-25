61f3c79f5b4d3b67f63058b78f02309c708472c6
Javier Chavez <jachavez6@gmail.com>
2016-09-25 15:41:42 -0400

    Wrote new function that takes care of reading from multiple columns.
    The original functions [firstName, lastName, emails] were only commented
    out as backup.
    
    To get the new function working, a class Column was created that, when
    instantiated, sets the column the copy() function needs and sets a caps
    variable to either True or False (so the copy() function can either
    capitalize names or skip if emails are being pulled)
    
    Feels pretty messy but the code currently works.

--- app.py
+++ app.py
@@ -7,10 +7,45 @@ sh = wb.sheet_by_index(0)
 total_rows = sh.nrows
 first_row = 6
 
-fname = []
+
+class Column():
+    def __init__(self, col, caps):  # col = column number, caps = True or False
+        self.col = col
+        self.caps = caps
+
+x = Column(2, True)  # first name
+y = Column(1, True)  # last name
+z = Column(3, False)  # email
+
+
+def copy(variable):
+    rowx = 6
+    colx = variable.col
+    temp_list = []
+    for row in range(first_row, total_rows):
+        if not sh.cell_value(rowx, colx=4):
+            rowx += 1
+        else:
+            temp_list.append(sh.cell_value(rowx, colx))
+            rowx += 1
+    if variable.caps:
+        final_list = [name.capitalize() for name in temp_list]
+        print('The capitalized list printed.')
+        print(final_list)
+        return final_list
+    else:
+        print('The temp_list printed.')
+        print(temp_list)
+        return temp_list
+
+# copy(x)
+# copy(y)
+# copy(z)
+"""
 def firstName():
     rowx = 6
     colx = 2
+    fname = []
     for row in range(first_row, total_rows):
         if not sh.cell_value(rowx, colx=4):
             rowx += 1
@@ -20,8 +55,9 @@ def firstName():
     cap_names = [name.capitalize() for name in fname]
     return cap_names
 
-lname = []
+
 def lastName():
+    lname = []
     rowx = 6
     colx = 1
     for row in range(first_row, total_rows):
@@ -33,8 +69,9 @@ def lastName():
     cap_sur = [name.capitalize() for name in lname]
     return cap_sur
 
-elist = []
+
 def email():
+    elist = []
     rowx = 6
     colx = 3
     for row in range(first_row, total_rows):
@@ -44,8 +81,8 @@ def email():
             elist.append(sh.cell_value(rowx, colx))
             rowx += 1
     return elist
-
-
+"""
+"""
 book = xlwt.Workbook()
 sheet = book.add_sheet('Email Adds')
 
@@ -75,3 +112,4 @@ wlname()
 wemail()
 
 book.save('Email Adds.xls')
+"""
