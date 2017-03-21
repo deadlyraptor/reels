63867220585f222d2c2700956039fd96f040da51
Javier Chavez <jachavez6@gmail.com>
2017-03-20 22:17:35 -0400

    Adds function to prep contacts for writing to spreadsheet

--- spreadchimp.py
+++ spreadchimp.py
@@ -35,7 +35,8 @@ def create_workbook():
     Returns:
         book = The workbook that was opened.
     '''
-    headers = ['First Name', 'Last Name', 'Email', 'Phone', 'Full address']
+    headers = ['Email', 'First Name', 'Last Name', 'Phone', 'Address 1',
+               'Address 2', 'City', 'State', 'Zip']
     book = xlwt.Workbook()
     sheet = book.add_sheet('Contacts')
     column_number = 0
@@ -58,5 +59,36 @@ def save_workbook(book, film):
     book.save('{0} contacts.xls'.format(film))
 
 
+# Preps the workbook that contains the information desired.
+wb = xlrd.open_workbook(workbook)
+sh = wb.sheet_by_index(0)
+total_rows = sh.nrows
+first_row = 6  # skips the first six rows as they are irrelevant.
+
+
+def prep_contacts():
+    contacts = []
+    for row in range(first_row, total_rows):
+        contact = []
+        row_values = sh.row_values(row)
+        opt_in = row_values[5]
+        film_title = row_values[20]
+        if not opt_in:
+            continue
+        elif opt_in and film_title in films:
+            contact = [row_values[3],  # email
+                       row_values[2].title(),  # first name
+                       row_values[1].title(),  # last name
+                       row_values[16].replace('No Primary Phone', ''),  # phone
+                       row_values[11],  # addrss 1
+                       row_values[12],  # address 2
+                       row_values[13].title(),  # city
+                       row_values[14],   # state
+                       row_values[15]]  # zip
+            contacts.append(contact)
+    return contacts
+
+
 for film in films:
     save_workbook(create_workbook(), film)
+    prep_contacts()
