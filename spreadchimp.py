c26ff99bbb1829dc243dc5215b5887cdf77a6998
Javier <jachavez6@gmail.com>
2020-09-28 10:39:05 -0400

    Split addresses into separate columns instead of combining them
    Combining them caused Mailchimp to insert a weird split that prevented
    the addresses from either saving or saving in the incorrect columns.

--- spreadchimp.py
+++ spreadchimp.py
@@ -4,9 +4,9 @@ import xlrd
 
 # Assumes the directory with the workbook is relative to the script's location.
 directory = 'workbooks/'
-file = os.listdir(directory)[0]
+thing = os.listdir(directory)[0]
 
-workbook = (f'{directory}/{file}')
+workbook = (f'{directory}/{thing}')
 
 # Preps the workbook that contains the information desired.
 wb = xlrd.open_workbook(workbook)
@@ -19,7 +19,7 @@ first_row = 6  # skips the first six rows as they are irrelevant.
 films = []
 for row in range(first_row, total_rows):
     row_values = sh.row_values(row)
-    film_title = row_values[20]
+    film_title = row_values[23]
     if film_title in films:
         pass
     else:
@@ -28,9 +28,9 @@ for row in range(first_row, total_rows):
 
 def prep_contacts(film):
     '''Collects all contacts that match the given film into a list that will be
-    used to write to the actual spreadsheet.
+  used to write to the actual spreadsheet.
 
-    Arguments:
+   Arguments:
         film = The film that contacts purchased tickets for.
 
     Returns:
@@ -40,31 +40,31 @@ def prep_contacts(film):
     for row in range(first_row, total_rows):
         contact = []
         row_values = sh.row_values(row)
-        opt_in = row_values[5]
-        film_title = row_values[20]
+        opt_in = row_values[8]
+        film_title = row_values[23]
         if not opt_in:
             continue
         elif opt_in and film_title == film:
-            address = '{0}  {1}  {2}  {3}  {4}'.format(
-                                                row_values[11],  # address 1
-                                                row_values[12],  # address 2
-                                                row_values[13].title(),  # city
-                                                row_values[14],  # state
-                                                row_values[15])  # zip
-            contact = [row_values[3],  # email
+            contact = [row_values[7],  # email
                        row_values[2].title(),  # first name
-                       row_values[1].title(),  # last name
-                       row_values[16].replace('No Primary Phone', ''),  # phone
-                       address]  # full address
+                       row_values[4].title(),  # last name
+                       row_values[19].replace('No Primary Phone', ''),  # phone
+                       row_values[14],  # address 1
+                       row_values[15],  # address 2
+                       row_values[16].title(),  # city
+                       row_values[17],  # state
+                       row_values[18]]  # zip
             contacts.append(contact)
     return contacts
 
 
-headers = ['Email', 'First Name', 'Last Name', 'Phone', 'Full Address']
+headers = ['Email', 'First Name', 'Last Name', 'Phone',
+           'Street Address', 'Address Line 2', 'City', 'State/Prov/Region',
+           'Zip/Postal']
 for film in films:
     contacts = prep_contacts(film)
-    with open(f'{film}.csv', mode='w') as outfile:
-        writer = csv.writer(outfile, delimiter=',', quotechar='"',
+    with open(f'{film}.csv', mode='w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_MINIMAL)
         writer.writerow(headers)
         for contact in contacts:
